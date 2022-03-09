5844bf47eccc48f475a50ceffa85cbb3e91daabf
Mayckon Barbosa <mayckonxp@gmail.com>
2022-03-09 13:24:57 -0300

    add bsc testnet to truffle config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,39 +1,44 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  networks: {},
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard BSC port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider(
+          PRIVATE_KEY,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    bsc: {
+      provider: () =>
+        new HDWalletProvider(PRIVATE_KEY, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.4", // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.8.4", // A version or constraint - Ex. "^0.5.0"
     },
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  // enabled: false,
-  // host: "127.0.0.1",
-  // adapter: {
-  //   name: "sqlite",
-  //   settings: {
-  //     directory: ".db"
-  //   }
-  // }
-  // }
 };
